1370c0589eb80faff5c20f2c6b8145bdf1bab0a0
jcondega <juanma199616@gmail.com>
2019-04-09 14:05:07 +0200

    comentarios

--- public/php/ficherosFormulario.php
+++ public/php/ficherosFormulario.php
@@ -9,16 +9,19 @@ class ficherosFormulario{
 
     function __construct()
     {
+        // comprobamos si existe el fichero para crearlo
         if(!file_exists($this->file)){
             fopen($this->file, "a");
             fclose($this->file);
         }
+        //cargamos el contenido del fichero en memoria
         $this->filecontent = file("../usuarios.txt");
     }
 
     function buscar($dni){
         $informacion = array();
 
+        // creamos un array bidimensional [registro][informacion]
         for($i = 0; $i < sizeof($this->filecontent); $i++){
             $registro = explode(",", $this->filecontent[$i]);
             for($j = 0; $j < sizeof($registro); $j++){
@@ -26,6 +29,7 @@ class ficherosFormulario{
             }
         }
 
+        // buscamos si el dni coincide en algun campo dni
         for ($i = 0; $i < sizeof($informacion[$i]); $i++){
             if($informacion[$i][0] == $dni){
                 return $informacion[$i];
@@ -40,6 +44,8 @@ class ficherosFormulario{
 
         $contador = 0;
 
+        //buscamos el dni y modificamos ese campo con los valores introducidos
+
         foreach ($this->filecontent as $datos){
             if($dni == substr($datos,0,strlen($dni))){
                 $this->filecontent[$contador] = $dni.','.$nombre.','.$apellidos.','.$telefono.','.$email.','.$direccion;
@@ -50,6 +56,8 @@ class ficherosFormulario{
         }
 
         $archivo = fopen("$this->file", "w+");
+
+        // reescribimos el fichero
 
         foreach ($this->filecontent as $registro){
             fwrite($archivo,$registro);
@@ -64,7 +72,7 @@ class ficherosFormulario{
     function darDeAlta($dni,$nombre,$apellidos,$telefono,$email,$direccion){
         $archivo = fopen("$this->file", "a");
 
-
+        // introducimos al final del fichero el nuevo registro
         fwrite($archivo,PHP_EOL.$dni.','.$nombre.','.$apellidos.','.$telefono.','.$email.','.$direccion);
         fclose($archivo);
     }
